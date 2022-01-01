3e47cf370c087841cc23e5d8e992f932345c51b6
Jo <88668777+jonars99@users.noreply.github.com>
2022-01-01 17:14:21 +0000

    track-mouse

--- catnip-game/src/Canvas.js
+++ catnip-game/src/Canvas.js
@@ -4,22 +4,37 @@ const Canvas = () => {
 
   const w = window.innerWidth;
   const h = window.innerHeight;
-  const canvas = useRef();
+  const cnvs = useRef();
 
-  React.useEffect( () => {
-    const ctx = canvas.current.getContext('2d');
+  useEffect( () => {
+    const ctx = cnvs.current.getContext('2d');
     ctx.beginPath();
     ctx.arc(100, 100, 20, 0, 2 * Math.PI);
     ctx.fill();
   });
+  
+  const mouseIsMoving = (e) => {
+    const canvas = document.getElementById('game-canvas');
+    const mousePos = mousePosition(canvas, e);
+    console.log("mouse is at: " + mousePos.x + ", " + mousePos.y);
+  }
+
+  const mousePosition = (canvas, e) => {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    }
+  }
 
   return (
     <div>
       <canvas
         id="game-canvas"
+        ref={cnvs}
         width={w}
         height={h}
-        ref={canvas}
+        onMouseMove={mouseIsMoving}
       ></canvas>
     </div>
   );
